c7c6f7c8353ce23e9ba3041c1bba7598ae13d8e4
Arturo Vela <velaarturo70@gmail.com>
2025-05-03 10:28:30 -0500

    Mejora

--- src/app/Components/About/About3.jsx
+++ src/app/Components/About/About3.jsx
@@ -16,7 +16,7 @@ const About3 = () => {
                 <div className="col-xl-12 text-center">
                     <h2 className="big-title mb-50 sm-mb-30 wow img-custom-anim-left">{aboutContent.title}</h2>
                     <p className="text mb-120 lg-mb-60 sm-mb-30">{parse(aboutContent.content)}</p>
-                    <figure className="thumb mb-0"><Image src={aboutContent.img} alt="img" width={1320} height={650}   /></figure>
+                    <figure className="thumb mb-0"><Image src={aboutContent.img} alt="img" width={1320} height={650} className="responsive-img"  /></figure>
                 </div>
             </div>
         </div>
