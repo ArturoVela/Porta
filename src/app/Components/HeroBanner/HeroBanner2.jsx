eb1a6131e1db5c48e8cc6c3dee895662c38f1132
Arturo Vela <velaarturo70@gmail.com>
2025-05-04 17:19:04 -0500

    Prioridad

--- src/app/Components/HeroBanner/HeroBanner2.jsx
+++ src/app/Components/HeroBanner/HeroBanner2.jsx
@@ -3,119 +3,186 @@ import Link from "next/link";
 import React from "react";
 
 const HeroBanner2 = () => {
+  const heroContent = {
+    number: "+51 963 653 154",
+    title2: " ",
+    title: "ARTURO VELA",
+    title3: "velaarturo70@gmail.com",
+    img: "/assets/images/hero/hero-img.jpg",
+    content:
+      "Soy ingeniero en sistemas y desarrollador web enfocado en crear soluciones prácticas, seguras y visualmente atractivas, combinando tecnología con diseño funcional.",
+    title5: "ING. SISTEMAS",
+    title6: "Con sede en perú",
+    title7: "Curriculum",
+  };
 
-
-    const heroContent = {
-        number:'+51 963 653 154',
-        title2:' ',
-        title:'ARTURO VELA',
-        title3:'velaarturo70@gmail.com',
-        img:'/assets/images/hero/hero-img.jpg',
-        content:'Soy ingeniero en sistemas y desarrollador web enfocado en crear soluciones prácticas, seguras y visualmente atractivas, combinando tecnología con diseño funcional.',
-        title5:'ING. SISTEMAS',
-        title6:'Con sede en perú',
-        title7:'Curriculum',
-      }
-
-    return (
-        <section className="hero-wrapper hero-1" id="hero">
-            <div className="hero-left-wrapper d-none d-xxl-block">
-                <div className="contact-number">
-                    <a href="tel:+519636531543">{heroContent.number}</a>
+  return (
+    <section className="hero-wrapper hero-1" id="hero">
+      <div className="hero-left-wrapper d-none d-xxl-block">
+        <div className="contact-number">
+          <a href="tel:+519636531543">{heroContent.number}</a>
+        </div>
+        <div className="hero-scroll smooth">
+          <a href="#about-area-1" id="hero-scroll">
+            <div className="scroll-me">Scroll</div>
+            <div className="hero-social_arrow">
+              <Image
+                src="/assets/images/icons/arrow-left-right.svg"
+                alt="img"
+                width={221}
+                height={51}
+              />
+            </div>
+          </a>
+        </div>
+      </div>
+      <div className="hero-social-wrapper d-none d-xxl-block">
+        <div className="follow-me">Sígueme</div>
+        <div className="hero-social_arrow mb-30">
+          <Image
+            src="/assets/images/icons/arrow-down-bottom.svg"
+            alt="img"
+            width={50}
+            height={220}
+          />
+        </div>
+        <div className="hero-social">
+          <div className="parallax-wrap">
+            <div className="parallax-element">
+              <a
+                className="facebook"
+                href="https://www.facebook.com/ArTuRoVeLaXD"
+                target="_blank"
+              >
+                <i className="bi bi-facebook"></i>
+              </a>
+            </div>
+          </div>
+          <div className="parallax-wrap">
+            <div className="parallax-element">
+              <a
+                className="twitter"
+                href="https://twitter.com/"
+                target="_blank"
+              >
+                <i className="bi bi-twitter"></i>
+              </a>
+            </div>
+          </div>
+          <div className="parallax-wrap">
+            <div className="parallax-element">
+              <a
+                className="instagram"
+                href="https://www.instagram.com/icchfinsa/"
+                target="_blank"
+              >
+                <i className="bi bi-instagram"></i>
+              </a>
+            </div>
+          </div>
+          <div className="parallax-wrap">
+            <div className="parallax-element">
+              <a
+                className="Linkedin"
+                href="https://www.linkedin.com/in/arturo-vela-aa81242ba/"
+                target="_blank"
+              >
+                <i className="bi bi-linkedin"></i>
+              </a>
+            </div>
+          </div>
+          <div className="parallax-wrap">
+            <div className="parallax-element">
+              <a
+                className="Discord"
+                href="https://discord.gg/mnMkUXC5vK"
+                target="_blank"
+              >
+                <i className="bi bi-discord"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="hero-style1">
+          <div className="row">
+            <div className="col-lg-12">
+              <h1
+                className="hero-title wow img-custom-anim-left"
+                data-wow-delay="0.1s"
+                data-wow-duration="1.5s"
+              >
+                {heroContent.title}
+                <div className="btn-wrap fade_right">
+                  <Link
+                    href="https://1drv.ms/b/s!AhTBs2k3EIAxjthfmoaQGB-VJxbvKA?e=jyDFsy"
+                    target="_blank"
+                    className="link-btn"
+                  >
+                    <span className="link-effect">
+                      <span className="effect-1">{heroContent.title7}</span>
+                      <span className="effect-1">{heroContent.title7}</span>
+                    </span>
+                    <Image
+                      src="/assets/images/icons/arrow-left-top.svg"
+                      alt="img"
+                      width={10}
+                      height={26}
+                    />
+                  </Link>
                 </div>
-                <div className="hero-scroll smooth">
-                    <a href="#about-area-1" id="hero-scroll">
-                        <div className="scroll-me">Scroll</div>
-                        <div className="hero-social_arrow">
-                        <Image src="/assets/images/icons/arrow-left-right.svg" alt="img" width={221} height={51}   />
-                        </div>
+              </h1>
+              <div className="row align-items-center justify-content-center">
+                <div className="col-lg-4">
+                  <div className="contact_mail">
+                    <a className="color1" href="mailto:velaarturo70@gmail.com">
+                      {heroContent.title3}
                     </a>
+                  </div>
                 </div>
+                <div className="col-lg-4">
+                  <div className="hero-thumb text-center">
+                    <Image
+                      src={heroContent.img}
+                      alt="img"
+                      width={360}
+                      height={240}
+                      priority={true}
+                      style={{ borderRadius: "30px" }}
+                    />
+                  </div>
+                </div>
+                <div className="col-lg-4">
+                  <p
+                    className="hero-text wow img-custom-anim-right"
+                    data-wow-delay="0.1s"
+                    data-wow-duration="1.5s"
+                  >
+                    {heroContent.content}
+                  </p>
+                </div>
+              </div>
+              <h1
+                className="hero-title font2 text-lg-end wow img-custom-anim-right"
+                data-wow-delay="0.1s"
+                data-wow-duration="1.7s"
+              >
+                {heroContent.title5}
+              </h1>
+              <p
+                className="place-title text-lg-end wow img-custom-anim-right"
+                data-wow-delay="0.1s"
+                data-wow-duration="1.7s"
+              >
+                {heroContent.title6}
+              </p>
             </div>
-            <div className="hero-social-wrapper d-none d-xxl-block">
-                <div className="follow-me">Sígueme</div>
-                <div className="hero-social_arrow mb-30">
-                <Image src="/assets/images/icons/arrow-down-bottom.svg" alt="img" width={50} height={220}   />
-                </div>
-                <div className="hero-social">
-                    <div className="parallax-wrap">
-                        <div className="parallax-element">
-                            <a className="facebook" href="https://www.facebook.com/ArTuRoVeLaXD" target="_blank">
-                            <i className="bi bi-facebook"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="parallax-wrap">
-                        <div className="parallax-element">
-                            <a className="twitter" href="https://twitter.com/" target="_blank">
-                            <i className="bi bi-twitter"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="parallax-wrap">
-                        <div className="parallax-element">
-                            <a className="instagram" href="https://www.instagram.com/icchfinsa/" target="_blank">
-                            <i className="bi bi-instagram"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="parallax-wrap">
-                        <div className="parallax-element">
-                            <a className="Linkedin" href="https://www.linkedin.com/in/arturo-vela-aa81242ba/" target="_blank">
-                            <i className="bi bi-linkedin"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="parallax-wrap">
-                        <div className="parallax-element">
-                            <a className="Discord" href="https://discord.gg/mnMkUXC5vK" target="_blank">
-                            <i className="bi bi-discord"></i>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="container">
-                <div className="hero-style1">
-                    <div className="row">
-                        <div className="col-lg-12">
-                            
-                            <h1 className="hero-title wow img-custom-anim-left" data-wow-delay="0.1s" data-wow-duration="1.5s">{heroContent.title} 
-                            <div className="btn-wrap fade_right">
-                                <Link href="https://1drv.ms/b/s!AhTBs2k3EIAxjthfmoaQGB-VJxbvKA?e=jyDFsy"  target="_blank" className="link-btn">
-                                    <span className="link-effect">
-                                        <span className="effect-1">{heroContent.title7}</span>
-                                        <span className="effect-1">{heroContent.title7}</span>
-                                    </span>
-                                    <Image src="/assets/images/icons/arrow-left-top.svg" alt="img" width={10} height={26}   />
-                                </Link>
-                            </div>
-                            </h1>
-                            <div className="row align-items-center justify-content-center">
-                                <div className="col-lg-4">
-                                    <div className="contact_mail">
-                                        <a className="color1" href="mailto:velaarturo70@gmail.com">{heroContent.title3}</a>
-                                    </div>
-                                </div>
-                                <div className="col-lg-4">
-                                    <div className="hero-thumb text-center">
-                                    <Image src={heroContent.img} alt="img" width={360} height={240}   style={{ borderRadius: '30px' }}/>
-                                    </div>
-                                </div>
-                                <div className="col-lg-4">
-                                    <p className="hero-text wow img-custom-anim-right" data-wow-delay="0.1s" data-wow-duration="1.5s">
-                                    {heroContent.content}
-                                    </p>
-                                </div>
-                            </div>
-                            <h1 className="hero-title font2 text-lg-end wow img-custom-anim-right" data-wow-delay="0.1s" data-wow-duration="1.7s">{heroContent.title5}</h1>
-                            <p className="place-title text-lg-end wow img-custom-anim-right" data-wow-delay="0.1s" data-wow-duration="1.7s">{heroContent.title6}</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default HeroBanner2;
