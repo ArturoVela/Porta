00062a4f141cf8e90e68ba6e47470067079ac085
Arturo Vela <velaarturo70@gmail.com>
2025-05-07 09:12:54 -0500

    Cambios

--- src/app/(defaultLayout)/pricing/page.jsx
+++ src/app/(defaultLayout)/pricing/page.jsx
@@ -1,11 +1,13 @@
 import Brand2 from '@/app/Components/Brand/Brand2';
 import Pricing3 from '@/app/Components/Pricing/Pricing3';
+import Pricing4 from '@/app/Components/Pricing/Pricing4';
 import React from 'react';
 
 const page = () => {
     return (
         <div>
             <Pricing3></Pricing3>
+            {/* <Pricing4></Pricing4> */}
             <Brand2></Brand2>              
         </div>
     );
